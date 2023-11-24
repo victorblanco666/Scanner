cd619026a83951f1dfcfae2d753ab03e72c41fa5
VB666 <vict.gamboa@duocuc.cl>
2023-11-24 19:53:22 -0300

    Integracion de camara

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-
+import { Camera, CameraResultType } from '@capacitor/camera';
+import { CameraSource } from '@capacitor/camera/dist/esm/definitions';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+
 export class HomePage {
 
+  imageSource:any;
   constructor() {}
+  
+  takePicture = async () =>{
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Prompt
+    })
+    this.imageSource = image.dataUrl
+  }
 
 }
